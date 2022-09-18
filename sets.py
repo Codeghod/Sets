33cf8eeb33bb9e2cd6b106e15b423cfff606996d
Alfred Turkson <111619866+Codeghod@users.noreply.github.com>
2022-09-18 15:25:22 -0700

    Add files via upload

---
+++ sets.py
@@ -0,0 +1,6 @@
+# A set is another container which stores values or keeps them in a curly bracket({}).
+# Similar to dictionaries but with sets a number can't be repeated
+# Sets deal with numbers.
+
+st = {12,43,56,8,3,9,7}
+print(st)
